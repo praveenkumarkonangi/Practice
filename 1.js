8b907c800e433ad153b6525ffff67d9adf5dcbf0
praveenkumarkonangi <praveenkumarkonangi4@gmail.com>
2021-11-26 13:52:54 +0530

    Practice

---
+++ 1.js
@@ -0,0 +1,15 @@
+class Employee{
+    emp=[{
+    name:"praveen",
+    sal:11,
+    loc:"kk",
+    },{
+    name:"pren",
+    sal:11,
+    loc:"kk",
+    
+}]
+}
+let e1=new Employee();
+let e2=new Employee();
+console.log(e1);
---
+++ index.html/1.js
@@ -0,0 +1,5 @@
+class Employee{
+    constructor(a,b,c){
+        console.log(a)
+    }
+}
